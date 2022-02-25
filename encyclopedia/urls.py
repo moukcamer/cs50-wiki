4e0022626f08b4400d24d2fb3e5d21e9f6f80540
moukcamer <46748251+moukcamer@users.noreply.github.com>
2022-02-25 11:08:31 +0100

    Update urls.py

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,8 +5,8 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
-    path("search", views.search, name="search"),
-    path("create", views.create, name="create"),
+    path("search/", views.search, name="search"),
+    path("create/", views.create, name="create"),
     path("edit/<str:title>", views.edit, name="edit"),
-    path("random_title", views.random_title, name="random_title"),
+    path("random_title/", views.random_title, name="random_title"),
 ]
