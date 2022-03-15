a740ca842a572396b764a61e2bfa108e4688e84e
moukcamer <46748251+moukcamer@users.noreply.github.com>
2022-03-15 08:38:16 +0100

    Update urls.py

--- wiki/urls.py
+++ wiki/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import include, path
 
@@ -21,3 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("encyclopedia.urls"))
 ]
+# Serve the static HTML
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
