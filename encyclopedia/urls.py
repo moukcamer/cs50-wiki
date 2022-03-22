5eb84479d70e5dd0b7612267e43daa7b0ff80b7f
moukcamer <46748251+moukcamer@users.noreply.github.com>
2022-03-22 14:31:04 +0100

    Update urls.py

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from . import views
-name_app= "encyclopedia"
+
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("entry/<str:title>", views.entry, name="entry"),
+    path('', views.index, name='index'),
+   path("entry/<str:title>", views.entry, name="entry"),
     path("search", views.search, name="search"),
     path("create", views.create, name="create"),
     path("edit/<str:title>", views.edit, name="edit"),
